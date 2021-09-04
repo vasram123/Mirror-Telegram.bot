42d510710b32ad868e1113342e2a95824a9117c4
Wi1mer <73017347+Wi1mer@users.noreply.github.com>
2021-09-03 23:29:39 -0500

    V 1.0.0.2.Spanish

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -24,7 +24,8 @@ class MirrorStatus:
 
 
 PROGRESS_MAX_SIZE = 100 // 8
-PROGRESS_INCOMPLETE = ['☆', '☆', '☆', '☆', '☆', '☆', '☆']
+# PROGRESS_INCOMPLETE = ['☆', '☆', '☆', '☆', '☆', '☆', '☆']
+PROGRESS_INCOMPLETE = ['🤍', '🤍', '🤍', '🤍', '🤍', '🤍', '🤍']
 
 SIZE_UNITS = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
 
@@ -81,7 +82,8 @@ def get_progress_bar_string(status):
     p = min(max(p, 0), 100)
     cFull = p // 8
     cPart = p % 8 - 1
-    p_str = '★' * cFull
+    # p_str = '★' * cFull
+    p_str = '❤️' * cFull
     if cPart >= 0:
         p_str += PROGRESS_INCOMPLETE[cPart]
     p_str += ' ' * (PROGRESS_MAX_SIZE - cFull)
@@ -93,15 +95,20 @@ def get_readable_message():
     with download_dict_lock:
         msg = ""
         for download in list(download_dict.values()):
-            msg += f"<b>Filename:</b> <code>{download.name()}</code>"
-            msg += f"\n<b>Status:</b> <i>{download.status()}</i>"
+            # msg += f"<b>Filename:</b> <code>{download.name()}</code>"
+            msg += f"<b>Nombre de Archivo:</b> <code>{download.name()}</code>"
+            # msg += f"\n<b>Status:</b> <i>{download.status()}</i>"
+            msg += f"\n<b>Estado:</b> <i>{download.status()}</i>"
             if download.status() != MirrorStatus.STATUS_ARCHIVING and download.status() != MirrorStatus.STATUS_EXTRACTING:
                 msg += f"\n<code>{get_progress_bar_string(download)} {download.progress()}</code>"
                 if download.status() == MirrorStatus.STATUS_DOWNLOADING:
-                    msg += f"\n<b>Downloaded:</b> {get_readable_file_size(download.processed_bytes())} of {download.size()}"
+                    # msg += f"\n<b>Downloaded:</b> {get_readable_file_size(download.processed_bytes())} of {download.size()}"
+                    msg += f"\n<b>Descargado:</b> {get_readable_file_size(download.processed_bytes())} de {download.size()}"
                 else:
-                    msg += f"\n<b>Uploaded:</b> {get_readable_file_size(download.processed_bytes())} of {download.size()}"
-                msg += f"\n<b>Speed :</b> {download.speed()}, \n<b>ETA:</b> {download.eta()} "
+                    # msg += f"\n<b>Uploaded:</b> {get_readable_file_size(download.processed_bytes())} of {download.size()}"
+                    msg += f"\n<b>Subido:</b> {get_readable_file_size(download.processed_bytes())} de {download.size()}"
+                # msg += f"\n<b>Speed :</b> {download.speed()}, \n<b>ETA:</b> {download.eta()} "
+                msg += f"\n<b>Velocidad :</b> {download.speed()}, \n<b>ETA:</b> {download.eta()} "
                 # if hasattr(download, 'is_torrent'):
                 try:
                     msg += f"\n<b>Seeders:</b> {download.aria_download().num_seeders}" \
